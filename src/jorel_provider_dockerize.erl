b9691379a32ae6293cae5020f6905da578e6f925
glejeune <gregoire.lejeune@free.fr>
2016-01-15 13:11:16 +0100

    [wip] Refactor dockerize

--- src/jorel_provider_dockerize.erl
+++ src/jorel_provider_dockerize.erl
@@ -6,18 +6,11 @@
 -export([init/1, do/1]).
 
 -define(PROVIDER, dockerize).
--define(PREINSTALL(JorelVersion), [
-                                   {run, "mkdir -p /opt"},
-                                   {label, jorel, JorelVersion}
-                                  ]).
--define(INSTALL(Output, AppName), [
-                           {workdir, "/opt"},
-                           {add, [filename:join([Output, AppName]), "/opt"]}
-                          ]).
--define(POSTINSTALL(AppName), [
-                               {workdir, "/opt/" ++ AppName},
-                               {cmd, "./bin/" ++ AppName ++ " start"}
-                              ]).
+-define(BUILD(Name), [
+                      {add, [".", "/tmp/" ++ Name]},
+                      {workdir, "/tmp/" ++ Name},
+                      {cmd, "make jorel.release"}
+                     ]).
 
 init(State) ->
   jorel_config:add_provider(
@@ -32,69 +25,105 @@ init(State) ->
    ).
 
 do(State) ->
+  case jorel_config:get(State, dockerize) of
+    {dockerize, []} ->
+      ?HALT("!!! Missing dockerize configuration", []);
+    {dockerize, Data} ->
+      dockerize(State, Data)
+  end.
+
+dockerize(State, Data) ->
   ?INFO("== Start provider ~p", [?PROVIDER]),
-  State1 = case jorel_config:get(State, dockerize, []) of
-             {dockerize, []} ->
-               ?HALT("!!! Missing dockerize configuration", []);
-             {dockerize, Data} ->
-               dockerize(State, Data)
-           end,
+  From = case lists:keyfind(from, 1, Data) of
+           false ->
+             ?HALT("!!! Missing from", []);
+           From1 ->
+             From1
+         end,
+  Maintainer = case lists:keyfind(maintainer, 1, Data) of
+                 false ->
+                   {maintainer, "Jorel"};
+                 Maintainer1 ->
+                   Maintainer1
+               end,
+  {Build, State1} = case lists:keyfind(build, 1, Data) of
+            {build, BuildConf} ->
+              build_in_docker(State, BuildConf, From, Maintainer);
+            _ ->
+              build(State)
+          end,
+
+  ?INFO("* Dockerize ~s", [Build]),
+  ?DEBUG("* TODO", []),
+
   ?INFO("== Provider ~p complete", [?PROVIDER]),
   State1.
 
-dockerize(State, Data) ->
-  Dockerfile = tempfile:name("Dockerfile.", [{ext, ""}]),
+build(State) ->
+  State1 = jorel_provider:run(State, release),
+  {outdir, Build} = jorel_config:get(State1, outdir),
+  {Build, State1}.
+
+build_in_docker(State, Conf, From, Maintainer) ->
+  Dockerfile = tempfile:name("Dockerfile.build.", [{ext, ""}]),
+  {relname, RelName} = jorel_config:get(State, relname),
   case file:open(Dockerfile, [write,binary]) of
     {ok, FD} ->
       ?INFO("* Create ~s", [Dockerfile]),
-      _ = create_dockerfile(FD, State, Data),
+      DockerfileData = [From, Maintainer] ++
+        buclists:keyfind(prebuild, 1, Conf, []) ++
+        ?BUILD(bucs:to_string(RelName)) ++
+        buclists:keyfind(postbuild, 1, Conf, []),
+      ?DEBUG("~p", [DockerfileData]),
+      _ = dockerfile(FD, DockerfileData),
       _ = file:close(FD);
     {error, Reason} ->
       ?HALT("!!! Can't create ~s: ~p", [Dockerfile, Reason])
   end,
-  ?INFO("* Create container", []),
-  _Password = string:strip(io:get_line("Password: "), both, $\n),
-  % ?DEBUG("* >> ~s <<", [Password]),
-  ?DEBUG("* sudo docker build --file=~s .", [Dockerfile]),
+  ?DEBUG("* docker build --file=~s -t erlang_dev .", [Dockerfile]),
+  ?DEBUG("* docker run --name tmp_build erlang_dev", []),
+  ?DEBUG("* docker cp tmp_build:/tmp/pipo/jorel.tar .", []),
+  ?DEBUG("* docker rm tmp_build", []),
+  ?DEBUG("* docker rmi erlang_dev", []),
+  Build = "TODO",
+  {Build, State}.
 
-  % TODO:
-  ?INFO("* Delete ~s", [Dockerfile]),
-  _ = file:delete(Dockerfile),
-  State.
+% dockerize(State, Data) ->
+%   Dockerfile = tempfile:name("Dockerfile.", [{ext, ""}]),
+%   case file:open(Dockerfile, [write,binary]) of
+%     {ok, FD} ->
+%       ?INFO("* Create ~s", [Dockerfile]),
+%       _ = create_dockerfile(FD, State, Data),
+%       _ = file:close(FD);
+%     {error, Reason} ->
+%       ?HALT("!!! Can't create ~s: ~p", [Dockerfile, Reason])
+%   end,
+%   ?INFO("* Create container", []),
+%   Password = string:strip(io:get_line("Password: "), both, $\n),
+%   % ?DEBUG("* >> ~s <<", [Password]),
+%   ?DEBUG("* echo \"~s\" | sudo -S docker build --file=~s .", [Password, Dockerfile]),
+% 
+%   % TODO:
+%   ?INFO("* Delete ~s", [Dockerfile]),
+%   _ = file:delete(Dockerfile),
+%   State.
 
-create_dockerfile(FD, State, Data) ->
-  create_dockerfile1(FD, State, Data),
-  {relname, Relname} = jorel_config:get(State, relname),
-  create_dockerfile1(FD, State, ?POSTINSTALL(bucs:to_string(Relname))).
-
-create_dockerfile1(_, _, []) -> ok;
-create_dockerfile1(FD, State, [{preinstall, PreInst}|Data]) ->
-  application:load(jorel),
-  {ok, Vsn} = application:get_key(jorel, vsn),
-  create_dockerfile1(FD, State, ?PREINSTALL(Vsn)),
-  create_dockerfile1(FD, State, PreInst),
-  create_dockerfile1(FD, State, Data);
-create_dockerfile1(FD, State, [{postinstall, PostInst}|Data]) ->
-  {output_dir, Output} = jorel_config:get(State, output_dir),
-  {relname, Relname} = jorel_config:get(State, relname),
-  create_dockerfile1(FD, State, ?INSTALL(Output, bucs:to_string(Relname))),
-  create_dockerfile1(FD, State, PostInst),
-  create_dockerfile1(FD, State, Data);
-create_dockerfile1(FD, State, [{label, Key, Value}|Data]) ->
+dockerfile(_, []) -> ok;
+dockerfile(FD, [{label, Key, Value}|Data]) ->
   file:write(FD, io_lib:format("LABEL ~s=\"~s\"~n", [Key, Value])),
-  create_dockerfile1(FD, State, Data);
-create_dockerfile1(FD, State, [{env, Key, Value}|Data]) ->
+  dockerfile(FD, Data);
+dockerfile(FD, [{env, Key, Value}|Data]) ->
   file:write(FD, io_lib:format("ENV ~s=~s~n", [Key, Value])),
-  create_dockerfile1(FD, State, Data);
-create_dockerfile1(FD, State, [{expose, Ports}|Data]) ->
+  dockerfile(FD, Data);
+dockerfile(FD, [{expose, Ports}|Data]) ->
   file:write(FD, io_lib:format("EXPOSE ~s~n", [string:join([bucs:to_string(E) || E <- Ports], " ")])),
-  create_dockerfile1(FD, State, Data);
-create_dockerfile1(FD, State, [{Cmd, Paths}|Data]) when Cmd == add; Cmd == copy ->
+  dockerfile(FD, Data);
+dockerfile(FD, [{Cmd, Paths}|Data]) when Cmd == add; Cmd == copy ->
   file:write(FD, io_lib:format("~s [~s]~n", 
                                [string:to_upper(bucs:to_string(Cmd)),
                                 string:join(["\"" ++ bucs:to_string(P) ++ "\"" || P <- Paths], ", ")])),
-  create_dockerfile1(FD, State, Data);
-create_dockerfile1(FD, State, [{Cmd, Param}|Data]) ->
+  dockerfile(FD, Data);
+dockerfile(FD, [{Cmd, Param}|Data]) ->
   file:write(FD, io_lib:format("~s ~s~n", [string:to_upper(bucs:to_string(Cmd)), Param])),
-  create_dockerfile1(FD, State, Data).
+  dockerfile(FD, Data).
 
