584bcf8e002b312696faa5999512a1320537f294
glejeune <gregoire.lejeune@free.fr>
2016-07-25 17:33:27 +0200

    Compile with Elixir

--- mix.exs
+++ mix.exs
@@ -8,7 +8,8 @@ defmodule Evic.Mixfile do
       elixir: "~> 1.2",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
-      deps: deps
+      deps: deps,
+      aliases: aliases
     ]
   end
 
@@ -29,4 +30,31 @@ defmodule Evic.Mixfile do
       {:jsx, "~> 2.8"}    
     ]
   end
+
+  defp aliases do
+    [compile: [&pre_compile_hooks/1, "compile", &post_compile_hooks/1]]
+  end
+
+  defp pre_compile_hooks(_) do
+    run_hook_cmd [
+      "make -C c_src -f Makefile.build"
+    ]
+  end
+
+  defp post_compile_hooks(_) do
+    run_hook_cmd [
+    ]
+  end
+
+  defp run_hook_cmd(commands) do
+    {_, os} = :os.type
+    for command <- commands, do: (fn
+      ({regex, cmd}) ->
+         if Regex.match?(Regex.compile!(regex), Atom.to_string(os)) do
+           Mix.Shell.cmd cmd, [], fn(x) -> Mix.Shell.IO.info(String.strip(x)) end
+         end
+      (cmd) ->
+        Mix.Shell.cmd cmd, [], fn(x) -> Mix.Shell.IO.info(String.strip(x)) end
+      end).(command)
+  end    
 end
