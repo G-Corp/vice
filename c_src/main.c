78588ffbfde802c2baf5fd4378809fd4d43f34f5
glejeune <gregoire.lejeune@free.fr>
2017-06-06 23:32:38 +0200

    Update NIF compilation
    Allow compile without -std=c99 or -std=gnu99 options

--- c_src/main.c
+++ c_src/main.c
@@ -3,17 +3,18 @@
 
 int main (int argc, char *argv[]) {
   s_detect f;
-  if(detect(argv[1], 
-        "/usr/share/opencv/haarcascades/haarcascade_frontalface_alt2.xml", 
+  size_t i, j;
+  if(detect(argv[1],
+        "/usr/share/opencv/haarcascades/haarcascade_frontalface_alt2.xml",
         "/usr/share/opencv/haarcascades/haarcascade_eye_tree_eyeglasses.xml",
         &f) == 0) {
-    for(size_t i = 0; i < f.nb_faces; i++) {
+    for(i = 0; i < f.nb_faces; i++) {
       printf("FACE #%ld\n", i);
       printf("X = %d\n", f.faces[i].x);
       printf("Y = %d\n", f.faces[i].y);
       printf("WIDTH = %d\n", f.faces[i].width);
       printf("HEIGTH = %d\n", f.faces[i].height);
-      for(size_t j = 0; j < f.faces[i].nb_eyes; j++) {
+      for(j = 0; j < f.faces[i].nb_eyes; j++) {
         printf("--EYE #%ld\n", j);
         printf("  X = %d\n", f.faces[i].eyes[j].x);
         printf("  Y = %d\n", f.faces[i].eyes[j].y);
