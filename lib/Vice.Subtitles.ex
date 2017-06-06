7e9a04cb076d77de2c05a0f10039603b1f238960
glejeune <gregoire.lejeune@free.fr>
2017-06-06 17:12:55 +0200

    Add elixir binding Vice.Subtitles

---
+++ lib/Vice.Subtitles.ex
@@ -0,0 +1,21 @@
+# File: Vice.Subtitles.ex
+# This file was generated from vice_subtitles.beam
+# Using rebar3_elixir (https://github.com/botsunit/rebar3_elixir)
+# MODIFY IT AT YOUR OWN RISK AND ONLY IF YOU KNOW WHAT YOU ARE DOING!
+defmodule Vice.Subtitles do
+  def unquote(:"parse")(arg1) do
+    :erlang.apply(:"vice_subtitles", :"parse", [arg1])
+  end
+  def unquote(:"parse_file")(arg1) do
+    :erlang.apply(:"vice_subtitles", :"parse_file", [arg1])
+  end
+  def unquote(:"to_string")(arg1, arg2) do
+    :erlang.apply(:"vice_subtitles", :"to_string", [arg1, arg2])
+  end
+  def unquote(:"to_file")(arg1, arg2) do
+    :erlang.apply(:"vice_subtitles", :"to_file", [arg1, arg2])
+  end
+  def unquote(:"to_file")(arg1, arg2, arg3) do
+    :erlang.apply(:"vice_subtitles", :"to_file", [arg1, arg2, arg3])
+  end
+end
