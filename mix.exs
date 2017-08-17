d832ab3213423cd072764b66bc9c59939e0b7087
glejeune <gregoire.lejeune@free.fr>
2017-08-17 12:57:03 +0200

    Update mix and rebar

--- mix.exs
+++ mix.exs
@@ -27,7 +27,7 @@ defmodule Vice.Mixfile do
       {:bucs, "~> 1.0.9"},
       {:doteki, "~> 1.0.5"},
       {:poolgirl, "~> 1.1.2"},
-      {:jsx, "~> 2.8.0"}    
+      {:jsx, "~> 2.8.0"}
     ]
   end
 
@@ -58,10 +58,18 @@ defmodule Vice.Mixfile do
     for command <- commands, do: (fn
       ({regex, cmd}) ->
          if Regex.match?(Regex.compile!(regex), Atom.to_string(os)) do
-           Mix.Shell.cmd cmd, [], fn(x) -> Mix.Shell.IO.info(String.strip(x)) end
+           Mix.Shell.cmd cmd, [], fn(x) -> Mix.Shell.IO.info(trim(x)) end
          end
       (cmd) ->
-        Mix.Shell.cmd cmd, [], fn(x) -> Mix.Shell.IO.info(String.strip(x)) end
+        Mix.Shell.cmd cmd, [], fn(x) -> Mix.Shell.IO.info(trim(x)) end
       end).(command)
-  end    
+  end
+
+  defp trim(x) do
+    if Version.compare(System.version, "1.5.0") == :lt do
+      String.strip(x)
+    else
+      String.trim(x)
+    end
+  end
 end
